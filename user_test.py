49f97cb9d4c6af5eeaebb30ec6463019c5eb860e
jLuseno161 <joyluseno2@gmail.com>
2021-05-28 21:48:58 +0300

    add setup method

--- user_test.py
+++ user_test.py
@@ -1,4 +1,18 @@
 
 import unittest
 from user import User
+
+
 class LoginTest(unittest.TestCase):
+    """
+        Test subclass that inherits from the test case.
+        Helps in creating tests
+    """
+
+# Test 1 - check for correct instantion of objects
+
+    def setUp(self):
+        """
+            setUp() method allows us to define instructions that will 
+            be executed before each test method
+        """
