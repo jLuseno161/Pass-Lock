40d86f2f3861c445b0ee90083c54d1781c13c606
jLuseno161 <joyluseno2@gmail.com>
2021-05-29 12:21:58 +0300

    save credentials

--- credential.py
+++ credential.py
@@ -1,13 +1,17 @@
 class Credential:
     """
-        Class generates new instance of credential
+        Class generates new instance of credentials
     """
 
-    credentia_list = []
+    credential_list = []
 
     def __init__(self,account_name, username, password):
         self.account_name = account_name
         self.username = username
         self.password = password
 
-        
+    def save_credentials(self):
+        Credential.credential_list.append(self)
+
+    
+     
